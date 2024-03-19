76b7313294afe677863a7ea3209f2125d97253b1
aahlaad26 <aahlaad26092002@gmail.com>
2024-03-19 23:45:06 +0530

    Adding userIDS to a list

--- Socialmedia/Views/Groups/AddNewGroupView.swift
+++ Socialmedia/Views/Groups/AddNewGroupView.swift
@@ -6,27 +6,42 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseStorage
+import FirebaseFirestore
 
+
+class UserIDs:ObservableObject{
+    @Published var userIDs = [String]()
+    func addUser(name: String) {
+        userIDs.append(name)
+    }
+}
 struct AddNewGroupView: View {
     @Environment(\.dismiss) private var dismiss
     @State private var groupSubject: String = ""
     private var isFormValid: Bool{
         !groupSubject.isEmpty
     }
+    @ObservedObject var ID = UserIDs()
     private func saveGroup(){
         let group = Groupped(subject: groupSubject)
     }
     var body: some View {
         NavigationStack{
             VStack{
-                HStack{
-                    TextField("Group Subject", text: $groupSubject)
-                }
+                
                 Spacer()
+                SearchUserGrpsView()
             }.toolbar{
                 ToolbarItem(placement: .principal){
-                    Text("New Group")
-                        .bold()
+                    HStack{
+                        TextField("Group Subject", text: $groupSubject)
+                            .frame(width: 150)
+                            .padding()
+                    }
+
+                    
                 }
                 ToolbarItem(placement: .navigationBarLeading){
                     Button("Cancel"){
