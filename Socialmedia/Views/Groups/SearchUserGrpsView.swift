97c851a216583e7462b1ed74a9f5b2f45795e44e
aahlaad26 <aahlaad26092002@gmail.com>
2024-03-19 23:44:40 +0530

    Adding userIds to a list

---
+++ Socialmedia/Views/Groups/SearchUserGrpsView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchUserGrpsView.swift
+//  Socialmedia
+//
+//  Created by user2 on 19/03/24.
+//
+
+import SwiftUI
+
+struct SearchUserGrpsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchUserGrpsView()
+}
