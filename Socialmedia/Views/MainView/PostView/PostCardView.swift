6e9a9479aa887625a86c853c4782f4c4032e24e2
aahlaad26 <aahlaad26092002@gmail.com>
2024-03-25 23:30:56 +0530

    Player added to postCardView As well

--- Socialmedia/Views/MainView/PostView/PostCardView.swift
+++ Socialmedia/Views/MainView/PostView/PostCardView.swift
@@ -14,7 +14,7 @@ struct PostCardView: View {
     //view properties
     @AppStorage("user_UID") private var userUID: String = ""
     @State private var docListener: ListenerRegistration? //for live updates
-
+    @State private var isPresented = false
     var body: some View {
         VStack{
             HStack(alignment: .top, spacing: 12){
@@ -74,7 +74,10 @@ struct PostCardView: View {
                                     
                                 }
                                 Spacer()
-                                PlayerView(player: $player, url: songURL)
+                                Button(action: {
+                                isPresented = true}) {
+                                    Image(systemName:"play.fill")
+                                }
                             }.padding()
                             .background(Color("bg-color"))
                             .clipShape(RoundedRectangle(cornerRadius: 10))
@@ -84,6 +87,10 @@ struct PostCardView: View {
                     
                     PostInteraction()
                 }
+        }.sheet(isPresented: $isPresented) {
+            // ProPlayer view presentation
+            ProPlayer2(post: post)
+
         }
         .frame(width: UIScreen.main.bounds.width-50)
         .padding()
@@ -251,3 +258,149 @@ struct AudioPlayerControlsView: View {
         }
     }
 }
+//
+//  ProPlayer.swift
+//  Socialmedia
+//
+//  Created by user2 on 23/03/24.
+//
+
+
+struct ProPlayer2: View {
+    @State var isPlaying: Bool = true
+    @State var post: Post
+    @State var player: AVPlayer?
+    @State private var width : CGFloat = UIScreen.main.bounds.height < 750 ? 130 : 230
+    @State private var angle : Double = 0
+    @State private var currentTimeString: String = ""
+    var body: some View {
+        NavigationView {
+            VStack {
+                //Slider------------------------------
+                ZStack{
+                    WebImage(url: post.imageURL)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: width,height: width)
+                        .clipShape(Circle())
+                    ZStack{
+                        Circle()
+                            .trim(from: 0,to: 0.8)
+                            .stroke(Color.black.opacity(0.06),lineWidth: 4)
+                            .frame(width: width + 45, height: width + 45)
+                        Circle()
+                            .trim(from: 0,to: CGFloat(angle)/360)
+                            .stroke(Color("button2-color"),lineWidth: 4)
+                            .frame(width: width + 45, height: width + 45)
+                        Circle()
+                            .fill(Color("button2-color"))
+                            .frame(width: 25,height: 25)
+                            .offset(x: (width+45)/2)
+                            .rotationEffect(.init(degrees: angle))
+                            .gesture(DragGesture().onChanged(onSlide(value:)))
+                    }
+                    .rotationEffect(.init(degrees: 126))
+                }
+                // Slider--------------------
+                Text("\(currentTimeString)")
+                Text(post.text)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.black)
+                    .padding(.top,15)
+                    .padding(.horizontal)
+                Text(post.username)
+                    .foregroundStyle(Color.gray)
+                    .padding(.vertical,10)
+                    .padding(.horizontal)
+                HStack {
+                    
+                    
+                    Button(action: self.playPause) {
+                        Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50, alignment: .center)
+                            .padding()
+                            .foregroundColor(Color("button2-color"))
+                    }
+                    
+                }
+            }
+            .onAppear() {
+                self.playSong()
+            }
+            .onReceive(Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()){
+                _ in updateProgress()
+            }
+        }
+        .background(Color("bg-color"))
+        .ignoresSafeArea()
+    }
+    func onSlide(value: DragGesture.Value) {
+        let vector = CGVector(dx: value.location.x, dy: value.location.y)
+        let radians = atan2(vector.dy - 12.5, vector.dx - 12.5)
+        var tempAngle = radians * 180 / .pi
+        tempAngle = tempAngle < 0 ? 360 + tempAngle : tempAngle
+        let maxAngle: CGFloat = 288 // Maximum angle representing 100% progress
+        
+        if tempAngle <= maxAngle {
+            self.angle = Double(tempAngle)
+            let progress = tempAngle / maxAngle
+            if let duration = player?.currentItem?.duration {
+                
+                
+                let totalSeconds = CMTimeGetSeconds(duration)
+                let targetTime = totalSeconds * Double(progress)
+                player?.seek(to: CMTime(seconds: targetTime, preferredTimescale: 1))
+        
+            }
+        }
+    }
+
+    
+    func playSong() {
+        guard let audioURL = post.songURL else {
+            print("Error: Audio URL is nil.")
+            return
+        }
+        
+        
+        let asset = AVAsset(url: audioURL)
+        let durationSeconds = CMTimeGetSeconds(asset.duration)
+        let minutes = Int(durationSeconds / 60)
+        let seconds = Int(durationSeconds.truncatingRemainder(dividingBy: 60))
+        print("Song Duration: \(minutes) minutes \(seconds) seconds")
+        
+        player = AVPlayer(playerItem: AVPlayerItem(url: audioURL))
+        player?.play()
+        
+    }
+    
+    func playPause() {
+        isPlaying.toggle()
+        if isPlaying == false {
+            player?.pause()
+        } else {
+            player?.play()
+        }
+    }
+    
+    func updateProgress(){
+        guard let totalTime = player?.currentItem?.duration
+        else{return}
+        if let currentTime = (player?.currentItem?.currentTime()){
+            let currentInSecs = CMTimeGetSeconds(currentTime)
+            let totalSec = CMTimeGetSeconds(totalTime)
+            self.angle = 288 * currentInSecs/totalSec
+            self.currentTimeString = timeString(time: currentInSecs)
+        }
+    }
+    func timeString(time: Double) -> String{
+        
+        let minutes = Int(time / 60)
+        let seconds = Int(time.truncatingRemainder(dividingBy: 60))
+        return String(format :"%02d:%02d",minutes,seconds)
+    }
+}
+
+
