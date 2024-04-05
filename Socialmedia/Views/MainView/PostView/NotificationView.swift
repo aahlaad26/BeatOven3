3b524edd7324f0949a313abcb66b579c01e82d89
mkswagger <mathangykrishna16@gmail.com>
2024-04-05 10:54:31 +0530

    added notifs page

---
+++ Socialmedia/Views/MainView/PostView/NotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationView.swift
+//  Socialmedia
+//
+//  Created by mathangy on 05/04/24.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Notification goes here")
+    }
+}
+
+#Preview {
+    NotificationView()
+}
